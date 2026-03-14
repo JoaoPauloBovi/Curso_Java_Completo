0e072892cccd458e39045398748769e614add13d
JoaoPauloBovi <joaopaulobovileal@gmail.com>
2026-03-14 00:13:19 -0300

    atualizaçao do curso

---
+++ src/Application/Area.java
@@ -0,0 +1,41 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Dimensoes do triangulo X: ");
+        double xA = entrada.nextDouble();
+        double xB = entrada.nextDouble();
+        double xC = entrada.nextDouble();
+
+        System.out.println("Dimensoes do triangulo Y: ");
+        double yA = entrada.nextDouble();
+        double yB = entrada.nextDouble();
+        double yC = entrada.nextDouble();
+
+        double P = (xA + xB + xC) / (double) 2.0F;
+
+        double areaX = Math.sqrt(P * (P - xA) * (P - xB) * (P - xC));
+
+        P = (yA + yB + yC) / (double) 2.0F;
+
+        double areaY = Math.sqrt(P * (P - xA) * (P - xB) * (P - xC));
+
+        System.out.printf("Area do triangulo X: %.2f%n ", areaX);
+        System.out.printf("Area do triangulo Y: %.2f%n ", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Maior Área X");
+
+        } else {
+            System.out.println("Maior Área Y");
+
+        }
+
+        entrada.close();
+    }
+}
