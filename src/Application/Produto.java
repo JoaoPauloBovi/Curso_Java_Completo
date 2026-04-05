e287c55604678b1f25e43bf548492e60d950c572
JoaoPauloBovi <joaopaulobovileal@gmail.com>
2026-04-04 22:09:38 -0300

    exercicios

--- src/Application/Produto.java
+++ src/Application/Produto.java
@@ -4,4 +4,24 @@ public class Produto {
     String nome;
     double preco;
     double desconto;
+
+
+//    Produto() { Construtor Padrão
+//    }
+
+    Produto(String nomeInicial) {
+        nome = nomeInicial;
+    }
+
+    Produto(String nomeInial, double precoInicial) {
+        nome = nomeInial;
+        preco = precoInicial;
+    }
+
+    Produto(String nomeInial, double precoInicial, double descontoInicial) {
+        nome = nomeInial;
+        preco = precoInicial;
+        desconto = descontoInicial;
+    }
+
 }
--- src/Application/ProdutoTeste.java
+++ src/Application/ProdutoTeste.java
@@ -3,25 +3,30 @@ package Application;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto();
-        p1.nome = "Notebook";
+        Produto p1 = new Produto("Notebook");
         p1.preco = 4356.89;
         p1.desconto = 0.25;
 
-        var p2 = new Produto();
+        Produto p2 = new Produto("Notebook", 4500.20);
+        p2.desconto = 0.25;
+
+        Produto p3 = new Produto("Notebook", 4500.20, 0.5);
+
+        /* var p3 = new Produto();
         p2.nome = "Caneta";
         p2.preco = 12.50;
-        p2.desconto = 0.29;
+        p2.desconto = 0.29; */
 
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
         double precoFinal = p1.preco * (1 - p1.desconto);
         double precoFinal2 = p2.preco * (1 - p2.desconto);
+        double precoFinal3 = p3.preco * (1 - p3.desconto);
 
         System.out.printf("Preco: %.2f%n", precoFinal);
         System.out.printf("Preco: %.2f%n", precoFinal2);
+        System.out.printf("Preco: %.2f%n", precoFinal3);
 
     }
-
 }
