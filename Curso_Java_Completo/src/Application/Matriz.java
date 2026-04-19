9afd658e063dff605412ddee766732ed2a7b9e12
JoaoPauloBovi <joaopaulobovileal@gmail.com>
2026-04-19 17:46:21 -0300

    exercicios

---
+++ Curso_Java_Completo/src/Application/Matriz.java
@@ -0,0 +1,32 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Matriz {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Quantos alunos? ");
+        int qtdAlunos = entrada.nextInt();
+
+        System.out.println("Quantos notas p/ aluno? ");
+        int qtdNotas = entrada.nextInt();
+
+        double[][] notasTurma = new double[qtdAlunos][qtdNotas];
+
+        double total = 0;
+        for (int a = 0; a < notasTurma.length; a++) {
+            for (int n = 0; n < notasTurma[a].length; n++) {
+                System.out.printf("Informe a nota %d do aluno %d: ", n, a);
+                notasTurma[a][n] = entrada.nextDouble();
+                total += notasTurma[a][n];
+            }
+        }
+
+        double media = total / (qtdAlunos * qtdNotas);
+        System.out.println("A media da nota dos aluno:" + media);
+
+        entrada.close();
+    }
+}
